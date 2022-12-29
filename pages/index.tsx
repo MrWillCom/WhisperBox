a53b909eee8672d3dbf10f4d5aefe771a4e7e39f
Mr. Will <mr.will.com@outlook.com>
2022-12-29 14:20:12 +0800

    Update Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Head from 'next/head'
 import Config from './_config'
 import styles from '../styles/Home.module.scss'
@@ -5,18 +6,50 @@ import styles from '../styles/Home.module.scss'
 import Intro from '../components/Intro'
 import Card from '../components/Card'
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>{Config.title}</title>
-        <meta name="description" content={Config.description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <Intro />
-      </main>
-    </>
-  )
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      questions: []
+    }
+  }
+
+  async fetchQuestions() {
+    const questions = await (await fetch('/api/fetch')).json()
+    this.setState({ questions })
+  }
+
+  componentDidMount() {
+    this.fetchQuestions()
+  }
+
+  render() {
+    return (
+      <>
+        <Head>
+          <title>{Config.title}</title>
+          <meta name="description" content={Config.description} />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={styles.main}>
+          <Intro />
+          {(() => {
+            var temp = []
+            for (let i = 0; i < this.state.questions.length; i++) {
+              const el = this.state.questions[i];
+              temp.push(<Card key={el.id}>
+                Q: {el.question}
+                {el.answer ? <><br />A: {el.answer}</> : ''}
+              </Card>)
+            }
+            return temp
+          })()}
+        </main>
+      </>
+    )
+  }
 }
+
+export default Home
